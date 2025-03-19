148a907bb0f8c1b7e8ac7a1c0f97d949e655689c
Leip Keat <yongyong967@hotmail.com>
2025-03-19 23:45:11 +0800

    Updated cloud function deployment shell script

--- src/real_time/inference_listener/setup_gcf.sh
+++ src/real_time/inference_listener/setup_gcf.sh
@@ -1,2 +1,2 @@
 #Incoming image listener
-gcloud functions deploy inference_listener-2 --gen2 --region=us-central1 --runtime=python311 --source=. --entry-point=subscribe --trigger-topic=incoming-image-topic --memory=1Gi
+gcloud functions deploy inference_listener-2 --gen2 --region=us-central1 --runtime=python311 --source=. --entry-point=subscribe --trigger-topic=incoming-image-topic --memory=2Gi
